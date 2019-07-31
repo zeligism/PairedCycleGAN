76310a72595b9758b5412122cda55f5536308d2f
Abzollo <ma7aseem@gmail.com>
2019-08-01 02:33:24 +0400

    The main script that runs the training process.

--- train.py
+++ train.py
@@ -1,112 +1,134 @@
 
 import os
-import time
+import pickle
 import argparse
 import torch
-from torch.data.utils import DataLoader
-from dataset.dataset import MakeupDataset, ToTensor
+import torchvision.transforms as transforms
+
+from dataset.dataset import MakeupDataset
+from dataset.transforms import SampleTransform
+from model.makeupnet import MakeupNet
+from trainer import MakeupNetTrainer
 
 
-RANDOM_SEED = 1
-CHANNEL_DIM = 0
-HEIGHT_DIM = 1
-WIDTH_DIM = 2
+### DEFAULT PARAMETERS ###
 
-# Dataset parameters
+# Random seed
+RANDOM_SEED = 123
+
+### Dataset parameters ###
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 DATASET_DIR = os.path.join(FILE_DIR, "dataset", "data", "processing", "faces")
-LANDMARKS_DIR = os.path.join(DATASET_DIR, "landmarks")
 
-# Training parameters
-OPTIMIZER = "sgd"
+### Network parameters ###
+NUM_CHANNELS = 3
+NUM_LATENT = 100
+NUM_FEATURES = 64
+
+### Training parameters ###
+NUM_GPU = 1
+NUM_EPOCHS = 5
 BATCH_SIZE = 4
+OPTIMIZER_NAME = "sgd"
 LEARNING_RATE = 1e-4
 MOMENTUM = 0.9
 CHECKPOINT = 1e4
-SHOULD_REPORT = lambda i, k: (i + 1) % k == 0
+
+### END ###
+
+MODEL_FNAME = "model/makeupnet.pickle"
 
 
-def train(model, dataset,
-	batch_size=4, lr=1e-4, optimizer="sgd", momentum=0.9, checkpoint=1e3):
+def save_model(model, fname=MODEL_FNAME):
+	"""
+	Saves the given model.
 
-	start_time = time.time()
-	
-	# Initialize device
-	_ = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-	# Create data loader
-	dataset_loader = DataLoader(dataset,
-		batch_size=batch_size, shuffle=True, num_workers=4)
-
-	# Initialize optimizer
-	if optimizer == "sgd":
-		optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum)
-	elif optimizer == "adam":
-		optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-	
-	# Define loss function @TODO
-	loss_function = lambda x, y: x - y
+	Args:
+		model: The model/net to be saved.
+		fname: The name of the pickle file containing the model.
+	"""
+	print("Saving model...")
+	with open(fname, "wb") as f:
+		pickle.dump(model, f)
 
 
-	# Define input and its true output
-	for index, sample in enumerate(dataset_loader):
+def load_model(fname=MODEL_FNAME):
+	"""
+	Saves the given model.
 
-		before = sample["before"]
-		after = sample["after"]
-		landmarks = sample["landmarks"]
+	Args:
+		fname: The name of the pickle file containing the model.
 
-		if dataset.with_landmarks():
-			before_mask = landmarks["before"] or torch.zeros(before.size())
-			after_mask = landmarks["after"] or torch.zeros(after.size())
-			torch.cat([before, before_mask], dim=CHANNEL_DIM)
-			torch.cat([after, after_mask], dim=CHANNEL_DIM)
-
-		# Do a forward pass, compute loss, then do a backward pass
-		after_pred = model(before)
-		loss = loss_function(after_pred, after)
-
-		# Zero grads, calculate them, and update
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
-
-		if SHOULD_REPORT(index, checkpoint):
-			print("[{}/{}] Loss = {:.3f}".format(index+1, len(dataset), loss.mean().item()))
-			print("Time elapsed = %ds" % (time.time() - start_time))
+	Returns:
+		The model loaded from `fname`.
+	"""
+	print("Loading model...")
+	with open(fname, "rb") as f:
+		return pickle.load(f)
 
 
 def main(args):
-	# Set random seed if given
+	"""
+	Trains the MakeupNet on MakeupDataset using MakeupNetTrainer.
+
+	Args:
+		args: The arguments passed from the command prompt (see below for more info).
+	"""
+
 	torch.manual_seed(args.random_seed or torch.initial_seed())
 
-	transform = ToTensor()
+	# Define data transformations
+	transform = transforms.Compose([
+		SampleTransform(transforms.Resize((64, 64))),
+		SampleTransform(transforms.ToTensor()),
+	])
+
+	# Define dataset parameters
 	dataset_params = {
 		"dataset_dir": args.dataset_dir,
 		"with_landmarks": args.with_landmarks,
 		"transform": transform,
 	}
 
-	# Choose dataset and initialize size of data's input and output
-	dataset = MakeupDataset(**dataset_params)
-
-	# Initialize model @TODO
-	model = ... # AutoMakeupNet(**model_params)
-
-	training_params = {
-		"batch_size": args.batch_size,
-		"lr": args.learning_rate,
-		"optimizer": args.optimizer,
-		"momentum": args.momentum,
-		"checkpoint": args.checkpoint,
+	# Define model parameters
+	model_params = {
+		"num_channels": args.num_channels,
+		"num_latent": args.num_latent,
+		"num_features": args.num_features,
 	}
-	train(model, dataset, **training_params)
+
+	# Define training parameters
+	training_params = {
+		"num_gpu": args.num_gpu,
+		"num_epochs": args.num_epochs,
+		"batch_size": args.batch_size,
+		"optimizer_name": args.optimizer_name,
+		"lr": args.learning_rate,
+		"momentum": args.momentum,
+		#"checkpoint": args.checkpoint,
+	}
+
+	# Start initializing dataset, model, and trainer
+	dataset = MakeupDataset(**dataset_params)
+	model = MakeupNet(**model_params)
+	trainer = MakeupNetTrainer(model, dataset, **training_params)
+
+	# Train MakeupNet
+	try:
+		trainer.train()
+	except (Exception, KeyboardInterrupt):
+		# @TODO: wait?
+		save_model(model)
+		raise
+
+	save_model(model)
 
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(description="train AutoMakeupNet on MakeupDataset.")
+	parser = argparse.ArgumentParser(description="train MakeupNet on MakeupDataset.")
 
-	parser.add_argument('--random_seed', type=int, default=RANDOM_SEED,
+	parser.add_argument("-rd", '--random_seed', type=int, default=RANDOM_SEED,
 		help="random seed (0 uses pytorch's initial seed).")
 
 	parser.add_argument('--dataset_dir', type=str, default=DATASET_DIR,
@@ -114,14 +136,26 @@ if __name__ == '__main__':
 	parser.add_argument('--with_landmarks', action="store_true",
 		help="use faces landmarks in training as well.")
 	
+	parser.add_argument('--num_channels', type=int, default=NUM_CHANNELS,
+		help="number of image channels in the dataset.")
+	parser.add_argument('--num_latent', type=int, default=NUM_LATENT,
+		help="number of latent factors from which an image will be generated.")
+	parser.add_argument('--num_features', type=int, default=NUM_FEATURES,
+		help="number of features on the layers of the discriminator (and the generator as well).")
+
+	parser.add_argument('--num_gpu', type=int, default=NUM_GPU,
+		help="number of GPUs to use, if any.")
+	parser.add_argument('--num_epochs', type=int, default=NUM_EPOCHS,
+		help="number of training epochs (i.e. full runs on the dataset).")
 	parser.add_argument('--batch_size', type=int, default=BATCH_SIZE,
 		help="batch size.")
+	parser.add_argument('--optimizer_name', type=str.lower, default=OPTIMIZER_NAME,
+		choices=("sgd", "adam", "rmsprop"),
+		help="the name of the optimizer used for training (SGD, Adam, etc.)")
 	parser.add_argument('--learning_rate', type=float, default=LEARNING_RATE,
-		help="learning rate.")
-	parser.add_argument('--optimizer', type=str.lower, default=OPTIMIZER, choices=("SGD", "Adam"),
-		help="the optimizer used for training (SGD, Adam, etc.)")
+		help="the learning rate used in the optimizer.")
 	parser.add_argument('--momentum', type=float, default=MOMENTUM,
-		help="the momentum used in SGD optimizer.")
+		help="the momentum used in the optimizer, if applicable.")
 	parser.add_argument('--checkpoint', type=float, default=CHECKPOINT,
 		help="a report will be printed at every iteration that is divisible by the checkpoint index.")
 	
