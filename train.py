1e1020dd3b7e9b049a9c50699418603f2ed6d33f
Abzollo <ma7aseem@gmail.com>
2019-08-04 10:58:06 +0400

    updated the arguments accordingly and added extra input validations of the ranges of the input (if the input is a number)

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from dataset.transforms import MakeupSampleTransform
 from model.makeupnet import MakeupNet
 from trainer import MakeupNetTrainer
 
+# @TODO: replace tabs with spaces in all files
 
 ### DEFAULT PARAMETERS ###
 
@@ -39,7 +40,8 @@ MOMENTUM = 0.9
 BETAS = (0.9, 0.999)
 
 GAN_TYPE = "gan"
-CLIP = 0.01
+D_ITER = 5
+CLAMP = (-0.01, 0.01)
 GRADIENT_PENALTY_COEFF = 10.
 
 STATS_REPORT_INTERVAL = 50
@@ -58,8 +60,7 @@ def main(args):
 		args: The arguments passed from the command prompt (see below for more info).
 	"""
 
-	# @TODO: Validate input
-
+	# Unpack args and pass them to training objects
 	torch.manual_seed(args.random_seed or torch.initial_seed())
 
 	# Define data transformations
@@ -91,15 +92,16 @@ def main(args):
 		"num_gpu": args.num_gpu,
 		"num_workers": args.num_workers,
 		"batch_size": args.batch_size,
-		"optimizer_name": args.optimizer_name,
-		"lr": args.learning_rate,
+		"optimizer_name": args.optimizer,
+		"lr": args.lr,
 		"momentum": args.momentum,
 		"betas": tuple(args.betas),
 		"gan_type": args.gan_type,
-		"clip": args.clip,
-		"gp_coeff": args.gradient_penalty_coeff,
-		"stats_report_interval": args.stats_report_interval,
-		"progress_check_interval": args.progress_check_interval,
+		"clamp": tuple(args.clamp),
+		"gp_coeff": args.gp_coeff,
+		"stats_report_interval": args.stats_interval,
+		"progress_check_interval": args.progress_interval,
+		"debug_run": args.debug_run,
 	}
 
 	# Start initializing dataset, model, and trainer
@@ -111,72 +113,109 @@ def main(args):
 	trainer.run(num_epochs=args.num_epochs, save_results=args.save_results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description="train MakeupNet on MakeupDataset.")
 
-	parser.add_argument("-rd", '--random_seed', type=int, default=RANDOM_SEED,
-		help="random seed (0 uses pytorch's initial seed).")
+	parser.add_argument("--random_seed", type=int, default=RANDOM_SEED,
+		help="random seed (0 uses pytorch initial seed).")
 
 	### Dataset ###
-	parser.add_argument('--dataset_dir', type=str, default=DATASET_DIR,
+	parser.add_argument("--dataset_dir", type=str, default=DATASET_DIR,
 		help="directory of the makeup dataset.")
-	parser.add_argument('--with_landmarks', action="store_true",
+	parser.add_argument("--with_landmarks", action="store_true",
 		help="use faces landmarks in training as well.")
 	
 	### Model ###
-	parser.add_argument('--num_channels', type=int, default=NUM_CHANNELS,
+	parser.add_argument("--num_channels", type=int, default=NUM_CHANNELS,
 		help="number of image channels in the dataset.")
-	parser.add_argument('--num_latent', type=int, default=NUM_LATENT,
+	parser.add_argument("--num_latent", type=int, default=NUM_LATENT,
 		help="number of latent factors from which an image will be generated.")
-	parser.add_argument('--num_features', type=int, default=NUM_FEATURES,
+	parser.add_argument("--num_features", type=int, default=NUM_FEATURES,
 		help="number of features on the layers of the discriminator (and the generator as well).")
 
 	### Trainer ###
-	parser.add_argument('--trainer_name', type=str, default=TRAINER_NAME,
+	parser.add_argument("--trainer_name", type=str, default=TRAINER_NAME,
 		help="name of the model trainer (which is also the name of your experiment).")
-	parser.add_argument('--results_dir', type=str, default=RESULTS_DIR,
+	parser.add_argument("--results_dir", type=str, default=RESULTS_DIR,
 		help="directory where the results for each run will be saved.")
-	parser.add_argument('--load_model_path', type=str, default=LOAD_MODEL_PATH,
+	parser.add_argument("--load_model_path", type=str, default=LOAD_MODEL_PATH,
 		help="the path of the file where the model will be loaded and experiments will be saved.")
 	
-	parser.add_argument('--num_gpu', type=int, default=NUM_GPU,
+	parser.add_argument("--num_gpu", type=int, default=NUM_GPU,
 		help="number of GPUs to use, if any.")
-	parser.add_argument('--num_workers', type=int, default=NUM_WORKERS,
+	parser.add_argument("--num_workers", type=int, default=NUM_WORKERS,
 		help="number of workers that will be loading the dataset.")
-	parser.add_argument('--batch_size', type=int, default=BATCH_SIZE,
+	parser.add_argument("--batch_size", type=int, default=BATCH_SIZE,
 		help="size of the batch sample from the dataset.")
 
-	parser.add_argument('--optimizer_name', type=str.lower, default=OPTIMIZER_NAME,
+	parser.add_argument("--optimizer", type=str.lower, default=OPTIMIZER_NAME,
 		help="the name of the optimizer used for training (SGD, Adam, RMSProp)",
 		choices=("sgd", "adam", "rmsprop"),)
-	parser.add_argument('-lr', '--learning_rate', type=float, default=LEARNING_RATE,
+	parser.add_argument("--lr", type=float, default=LEARNING_RATE,
 		help="the learning rate, which controls the size of the optimization update.")
-	parser.add_argument('--momentum', type=float, default=MOMENTUM,
+	parser.add_argument("--momentum", type=float, default=MOMENTUM,
 		help="used in SGD and RMSProp optimizers.")
-	parser.add_argument('--betas', type=float, nargs="+", default=BETAS,
+	parser.add_argument("--betas", type=float, nargs=2, default=BETAS,
 		help="used in Adam optimizer (see torch.optim.Adam for details).")
 
-	parser.add_argument('--gan_type', type=str.lower, default=GAN_TYPE,
+	parser.add_argument("--gan_type", type=str.lower, default=GAN_TYPE,
 		choices=("gan", "wgan", "wgan-gp"),
 		help="type of gan among GAN (default), WGAN (Wasserstein GAN), and WGAN-GP (WGAN with gradient penalty).")
-	parser.add_argument('--clip', type=float, default=CLIP,
+	parser.add_argument("--D_iter", type=int, default=D_ITER,
+		help="number of iterations to train discriminator every batch.")
+	parser.add_argument("--clamp", type=float, nargs=2, default=CLAMP,
 		help="used in WGAN for clipping the weights of the discriminator to ensure 1-Lipschitzness.")
-	parser.add_argument('--gradient_penalty_coeff', type=float, default=GRADIENT_PENALTY_COEFF,
+	parser.add_argument("--gp_coeff", type=float, default=GRADIENT_PENALTY_COEFF,
 		help="a coefficient to multiply with the gradient penalty in the loss of WGAN-GP.")
 
-	parser.add_argument('--stats_report_interval', type=int, default=STATS_REPORT_INTERVAL,
+	parser.add_argument("--stats_interval", type=int, default=STATS_REPORT_INTERVAL,
 		help="the interval in which a report of the training stats will be shown to the console.")
-	parser.add_argument('--progress_check_interval', type=int, default=PROGRESS_CHECK_INTERVAL,
+	parser.add_argument("--progress_interval", type=int, default=PROGRESS_CHECK_INTERVAL,
 		help="the interval in which the progress of the generator will be checked and recorded.")
 
 	### Trainer.run() ###
-	parser.add_argument('--num_epochs', type=int, default=NUM_EPOCHS,
+	parser.add_argument("--num_epochs", type=int, default=NUM_EPOCHS,
 		help="number of training epochs (i.e. full runs on the dataset).")
-	parser.add_argument('--save_results', action="store_true",
+	parser.add_argument("--save_results", action="store_true",
 		help="save the results of the experiment.")
+	parser.add_argument("--debug_run", action="store_true",
+		help="just run trainer without reporting or saving anything.")
 	
+	# Parse arguments
 	args = parser.parse_args()
 
+	# Additional validation of input
+	if not (args.num_channels > 0):
+		parser.error("Number of channels should be a positive integer.")
+	if not (args.num_latent > 0):
+		parser.error("Number of latent dimensions should be a positive integer.")
+	if not (args.num_features > 0):
+		parser.error("Number of features per layer should be a positive integer.")
+
+	if not (args.num_gpu >= 0):
+		parser.error("Number of GPUs should be a non-negative integer.")
+	if not (args.num_workers >= 0):
+		parser.error("Number of data loading workers should be a non-negative integer.")
+	if not (args.batch_size > 0):
+		parser.error("Batch size should be a positive integer.")
+
+	if not (args.D_iter > 0):
+		parser.error("Number of discriminator iterations should be a positive integer.")
+	if not (args.momentum >= 0):
+		parser.error("Momentum should be a positive integer.")
+	for beta in args.betas:
+		if not (0. <= beta and beta < 1.):
+			parser.error("Betas should be numbers in the range [0, 1).")
+
+	if not (args.stats_interval > 0):
+		parser.error("Stats report interval should be a positive integer.")
+	if not (args.progress_interval > 0):
+		parser.error("Progress check interval should be a positive integer.")
+
+	if not (args.num_epochs > 0):
+		parser.error("Number of epochs should be a positive integer.")
+
+	# Run main function
 	main(args)
 
