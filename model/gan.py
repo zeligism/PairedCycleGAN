afdb93175dff9e8457f6d0e82b9a434425ec1515
Abzollo <ma7aseem@gmail.com>
2019-08-09 00:59:20 +0400

    seperated GAN modules from makeupnet and reduced repetitive blocks by defining a block module for both discriminator and generator

---
+++ model/gan.py
@@ -0,0 +1,100 @@
+
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    """The discriminator of the MakeupNet"""
+
+    def __init__(self, num_channels, num_features,
+                 depth=5, gan_type="gan", with_landmarks=False):
+        super().__init__()
+
+        using_gradient_penalty = gan_type == "wgan-gp"
+        use_batchnorm = not using_gradient_penalty
+
+        # input is num_channels x H x W
+        self.main = nn.Sequential(
+            DiscriminatorBlock(num_channels, num_features),
+            DiscriminatorBlock(num_features * 1, num_features * 2, use_batchnorm=use_batchnorm),
+            DiscriminatorBlock(num_features * 2, num_features * 4, use_batchnorm=use_batchnorm),
+            DiscriminatorBlock(num_features * 4, num_features * 8, use_batchnorm=use_batchnorm),
+            nn.Conv2d(num_features * 8, 1,
+                      kernel_size=4, stride=1, padding=0, bias=False),
+        )
+
+        if gan_type == "gan":
+            self.main.add_module("DiscriminatorSigmoid", nn.Sigmoid())
+
+
+    def forward(self, inputs):
+        return self.main(inputs).view(-1)
+
+
+class DiscriminatorBlock(nn.Module):
+    """
+    A discriminator convolutional block.
+    Default stride and padding half the size of features,
+    e.g. if input is [in_channels, 64, 64], output will be [out_channels, 32, 32].
+    """
+
+    def __init__(self, in_channels, out_channels, stride=2, padding=1, use_batchnorm=False):
+        super().__init__()
+
+        modules = []
+
+        modules.append(nn.Conv2d(in_channels, out_channels, 4, stride=2, padding=1, bias=False))
+        if use_batchnorm: modules.append(nn.BatchNorm2d(out_channels))
+        modules.append(nn.LeakyReLU(0.2, inplace=True))
+        
+        self.main = nn.Sequential(*modules)
+
+
+    def forward(self, x):
+        return self.main(x)
+
+
+class Generator(nn.Module):
+    """The generator of the MakeupNet"""
+
+    def __init__(self, num_latents, num_features, num_channels,
+                 depth=5, gan_type="gan", with_landmarks=False):
+        super().__init__()
+
+        self.main = nn.Sequential(
+            GeneratorBlock(num_latents, num_features * 8, stride=1, padding=0),
+            GeneratorBlock(num_features * 8, num_features * 4),
+            GeneratorBlock(num_features * 4, num_features * 2),
+            GeneratorBlock(num_features * 2, num_features * 1),
+            nn.ConvTranspose2d(num_features, num_channels,
+                               kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh(),
+        )
+
+
+    def forward(self, inputs):
+        inputs = inputs.view(*inputs.size(), 1, 1)  # add H and W dimensions
+        return self.main(inputs)
+
+
+class GeneratorBlock(nn.Module):
+    """
+    A generator convolutional block.
+    Default stride and padding double the size of features,
+    e.g. if input is [in_channels, 32, 32], output will be [out_channels, 64, 64].
+    """
+
+    def __init__(self, in_channels, out_channels, stride=2, padding=1):
+        super().__init__()
+
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4,
+                               stride=stride, padding=padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.Dropout(),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+
+    def forward(self, x):
+        return self.main(x)
+
--- model/makeupnet.py
+++ model/makeupnet.py
@@ -1,14 +1,15 @@
 
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+from .gan import Discriminator, Generator
 
 
 class MakeupNet(nn.Module):
     """The main module of the MakeupNet"""
 
-    def __init__(self, name="makeupnet",
-        num_channels=3, num_features=64, num_latent=100,
+    def __init__(self, name="MakeupNet",
+        num_channels=3, num_features=64, num_latents=128,
+        conv_std=0.02, batchnorm_std=0.02,
         depth=5, gan_type="gan", with_landmarks=False):
         """
         Initializes MakeupNet.
@@ -16,115 +17,39 @@ class MakeupNet(nn.Module):
         Args:
             num_channels: the number of channels in the input images.
             num_features: controls the numbers of filters in each conv/up-conv layer.
-            num_latent: the number of latent factors.
+            num_latents: the number of latent factors.
+            ... @TODO
         """
         super().__init__()
+
+        self.name = name
         self.num_channels = num_channels
         self.num_features = num_features
-        self.num_latent = num_latent
+        self.num_latents = num_latents
         self.with_landmarks = with_landmarks
+        self.conv_std = conv_std
+        self.batchnorm_std = batchnorm_std
 
-        self.G = Generator(num_channels, num_features, num_latent, with_landmarks)
-        self.D = Discriminator(num_channels, num_features, num_latent, with_landmarks)
-        self.weights_init()
+        self.D = Discriminator(num_channels=num_channels, num_features=num_features)
+        self.G = Generator(num_latents=num_latents, num_features=num_features, num_channels=num_channels)
+        self.weights_init(conv_std=0.02, batchnorm_std=0.02)
 
 
-    def weights_init(self, conv_sd=0.02, batchnorm_sd=0.02):
+    def weights_init(self, conv_std=0.02, batchnorm_std=0.02):
         """
         A method that initializes weights of `self` in-place.
 
         Args:
-            conv_sd: the standard deviation of the conv/up-conv layers.
-            batchnorm_sd: the standard deviation of the batch-norm layers.
+            conv_std: the standard deviation of the conv/up-conv layers.
+            batchnorm_std: the standard deviation of the batch-norm layers.
         """
         def weights_init_apply(module):
             classname = module.__class__.__name__
             if classname.find('Conv') != -1:
-                nn.init.normal_(module.weight.data, 0.0, conv_sd)
+                nn.init.normal_(module.weight.data, 0.0, conv_std)
             elif classname.find('BatchNorm') != -1:
-                nn.init.normal_(module.weight.data, 1.0, batchnorm_sd)
+                nn.init.normal_(module.weight.data, 1.0, batchnorm_std)
                 nn.init.constant_(module.bias.data, 0)
 
         self.apply(weights_init_apply)
-
-
-
-class Generator(nn.Module):
-    """The generator of the MakeupNet"""
-
-    def __init__(self, num_channels, num_features, num_latent,
-        depth=5, gan_type="gan", with_landmarks=False):
-        super().__init__()
-
-        modules = []
-
-        # Layer #1 (in): Generating from latent vector
-        modules.append(nn.ConvTranspose2d(num_latent, num_features * 8, 4, stride=1, padding=0, bias=False))
-        modules.append(nn.BatchNorm2d(num_features * 8))
-        modules.append(nn.ReLU(True))
-
-        # Layer #2: state size. (num_features*8) x 4 x 4
-        modules.append(nn.ConvTranspose2d(num_features * 8, num_features * 4, 4, stride=2, padding=1, bias=False))
-        modules.append(nn.BatchNorm2d(num_features * 4))
-        modules.append(nn.ReLU(True))
-
-        # Layer #3: state size. (num_features*4) x 8 x 8
-        modules.append(nn.ConvTranspose2d(num_features * 4, num_features * 2, 4, stride=2, padding=1, bias=False))
-        modules.append(nn.BatchNorm2d(num_features * 2))
-        modules.append(nn.ReLU(True))
-
-        # Layer #4: state size. (num_features*2) x 16 x 16
-        modules.append(nn.ConvTranspose2d(num_features * 2, num_features, 4, stride=2, padding=1, bias=False))
-        modules.append(nn.BatchNorm2d(num_features))
-        modules.append(nn.ReLU(True))
-
-        # Layer #5 (out): state size. (num_features) x 32 x 32
-        modules.append(nn.ConvTranspose2d(num_features, num_channels, 4, stride=2, padding=1, bias=False))
-        modules.append(nn.Tanh())
-
-        self.main = nn.Sequential(*modules)
-
-
-    def forward(self, inputs):
-        return self.main(inputs)
-
-
-class Discriminator(nn.Module):
-    """The discriminator of the MakeupNet"""
-
-    def __init__(self, num_channels, num_features, num_latent,
-        depth=5, gan_type="gan", with_landmarks=False):
-        super().__init__()
-
-        modules = []
-
-        # Layer #1 (in): input is (nc) x 64 x 64
-        modules.append(nn.Conv2d(num_channels, num_features, 4, stride=2, padding=1, bias=False))
-        modules.append(nn.LeakyReLU(0.2, inplace=True))
-
-        # Layer #2: state size. (num_features) x 32 x 32
-        modules.append(nn.Conv2d(num_features, num_features * 2, 4, stride=2, padding=1, bias=False))
-        if gan_type != "wgan-gp": modules.append(nn.BatchNorm2d(num_features * 2))
-        modules.append(nn.LeakyReLU(0.2, inplace=True))
-
-        # Layer #3: state size. (num_features*2) x 16 x 16
-        modules.append(nn.Conv2d(num_features * 2, num_features * 4, 4, stride=2, padding=1, bias=False))
-        if gan_type != "wgan-gp": modules.append(nn.BatchNorm2d(num_features * 4))
-        modules.append(nn.LeakyReLU(0.2, inplace=True))
-
-        # Layer #4: state size. (num_features*4) x 8 x 8
-        modules.append(nn.Conv2d(num_features * 4, num_features * 8, 4, stride=2, padding=1, bias=False))
-        if gan_type != "wgan-gp": modules.append(nn.BatchNorm2d(num_features * 8))
-        modules.append(nn.LeakyReLU(0.2, inplace=True))
-
-        # Layer #5 (out): state size. (num_features*8) x 4 x 4
-        modules.append(nn.Conv2d(num_features * 8, 1, 4, stride=1, padding=0, bias=False))
-        modules.append(nn.Sigmoid())
-
-        self.main = nn.Sequential(*modules)
-
-
-    def forward(self, inputs):
-        return self.main(inputs).view(-1)
-
 
