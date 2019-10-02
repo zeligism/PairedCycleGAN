f0e78f91cacdd0d01f6f87f09cb8adfcddc722dd
Abzollo <ma7aseem@gmail.com>
2019-10-02 22:33:59 +0400

    add config support + made args type checking better

--- src/train.py
+++ src/train.py
@@ -7,75 +7,79 @@ import torchvision.transforms as transforms
 
 from dataset.dataset import MakeupDataset
 from dataset.transforms import MakeupSampleTransform
-from model.makeupnet import MakeupNet
-from trainers.makeupnet_trainer import MakeupNetTrainer
+from model.makeupnet import _MakeupNet, MakeupNet
+from trainers.makeupnet_trainer import _MakeupNetTrainer, MakeupNetTrainer
 
 # @TODO: add logging
-# @TODO: put hyperparams in yaml
-"""
-with open("optim_configs.yaml") as f:
-    test = yaml.load(f)
-"""
 
-### DEFAULT HYPERPARAMETERS ###
-
-# Random seed
-RANDOM_SEED = 123
-
-### Dataset parameters ###
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 DATASET_DIR = os.path.join(FILE_DIR, "dataset", "data", "processing", "faces")
-IMAGE_SIZE = 64
 
-### Network parameters ###
-GAN_TYPE = "gan"
-NUM_CHANNELS = 3
-NUM_LATENTS = 128
-NUM_FEATURES = 64
 
-### Training parameters ###
-TRAINER_NAME = "trainer"
-RESULTS_DIR = "results/"
-LOAD_MODEL_PATH = None
+def load_config(config_key, config_file="config.yaml"):
+    with open(config_file) as f:
+        all_configs = yaml.load(f)
+        try:
+            return all_configs[config_key]
+        except KeyError:
+            exit(f"Config key '{config_key}' is not defined in config file '{config_file}'.")
 
-NUM_GPU = 1
-NUM_WORKERS = 2
-BATCH_SIZE = 4
 
-# Optimizer's hyperparameters are dependent on the type of gan.
-# Make sure you change these accordingly.
-# @TODO: Add separate hyperparams for D and G?
+def get_dataset_args(args):
+    # Construct dataset parameters from args
+    dataset_args = {
+        "dataset_dir": args.dataset_dir,
+        "with_landmarks": args.with_landmarks,
+    }
 
-OPTIMIZER_NAME = "sgd"
-LEARNING_RATE = 1e-4
-MOMENTUM = 0.
-BETAS = (0.9, 0.999)
+    return dataset_args
 
-if GAN_TYPE == "gan":
-    OPTIMIZER_NAME = "sgd"
-    LEARNING_RATE = 1e-4
 
-elif GAN_TYPE == "wgan":
-    OPTIMIZER_NAME = "rmsprop" # or adam
-    LEARNING_RATE = 5e-5
-    BETAS = (0.5, 0.9)
+def get_model_args(args):
+    # Construct model parameters from args
+    model_args = {
+        "num_latents": args.num_latents,
+        "num_features": args.num_features,
+        "image_channels": args.image_channels,
+        "image_size": args.image_size,
+        "gan_type": args.gan_type,
+    }
 
-elif GAN_TYPE == "wgan-gp":
-    OPTIMIZER_NAME = "adam"
-    LEARNING_RATE = 1e-4
-    BETAS = (0.0, 0.9)
+    return model_args
 
-D_ITERS = 5
-CLAMP = (-0.01, 0.01)  # for WGAN
-GRADIENT_PENALTY_COEFF = 10.  # for WGAN-GP
 
-# Checkpoints frequency
-STATS_REPORT_INTERVAL = 50
-PROGRESS_CHECK_INTERVAL = 200
+def get_trainer_args(args):
 
-NUM_EPOCHS = 5
+    # Construct training parameters from args
+    trainer_args = {
+        "name": args.trainer_name,
+        "results_dir": args.results_dir,
+        "load_model_path": args.load_model,
+        "num_gpu": args.num_gpu,
+        "num_workers": args.num_workers,
+        "batch_size": args.batch_size,
 
-### END ###
+        "D_optim_config": {
+            "optim_choice": args.D_optimizer,
+            "lr": args.D_lr,
+            "momentum": args.D_momentum,
+            "betas": args.D_betas,
+        },
+        "G_optim_config": {
+            "optim_choice": args.G_optimizer,
+            "lr": args.G_lr,
+            "momentum": args.G_momentum,
+            "betas": args.G_betas,
+        },
+
+        "D_iters": args.D_iters,
+        "clamp": args.clamp,
+        "gp_coeff": args.gp_coeff,
+        "stats_interval": args.stats_interval,
+        "generate_grid_interval": args.generate_grid_interval,
+    }
+
+    return trainer_args
 
 
 def main(args):
@@ -86,66 +90,30 @@ def main(args):
         args: The arguments passed from the command prompt (see below for more info).
     """
 
-    # Unpack args and pass them to training objects
-    torch.manual_seed(args.random_seed or torch.initial_seed())
+    torch.manual_seed(args.random_seed)
 
-    # Define data transformations
-    transform_list = list(map(MakeupSampleTransform, [
-        transforms.Resize((args.image_size, args.image_size)),
-        transforms.ColorJitter(brightness=0.1),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ]))
-
-    # Define dataset parameters
-    dataset_params = {
-        "dataset_dir": args.dataset_dir,
-        "with_landmarks": args.with_landmarks,
-        "transform": transforms.Compose(transform_list),
-    }
-
-    # Define model parameters
-    model_params = {
-        "num_latents": args.num_latents,
-        "num_features": args.num_features,
-        "image_channels": args.image_channels,
-        "image_size": args.image_size,
-        "gan_type": args.gan_type,
-    }
-
-    # Define training parameters
-    trainer_params = {
-        "name": args.trainer_name,
-        "results_dir": args.results_dir,
-        "load_model_path": args.load_model,
-        "num_gpu": args.num_gpu,
-        "num_workers": args.num_workers,
-        "batch_size": args.batch_size,
-
-        "D_optim_configs": {
-            "optim_choice": args.optimizer,
-            "lr": args.learning_rate,
-            "momentum": args.momentum,
-            "betas": tuple(args.betas),
-        },
-        "G_optim_configs": {
-            "optim_choice": args.optimizer,
-            "lr": args.learning_rate,
-            "momentum": args.momentum,
-            "betas": tuple(args.betas),
-        },
-
-        "D_iters": args.D_iters,
-        "clamp": tuple(args.clamp),
-        "gp_coeff": args.gp_coeff,
-        "stats_report_interval": args.stats_interval,
-        "progress_check_interval": args.progress_interval,
-    }
+    # Define data transformation
+    def make_transform(image_size):
+        transform_sequence = list(map(MakeupSampleTransform, [
+            transforms.Resize((image_size, image_size)),
+            transforms.ColorJitter(brightness=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]))
+        return transforms.Compose(transform_sequence)
 
     # Start initializing dataset, model, and trainer
-    dataset = MakeupDataset(**dataset_params)
-    model = MakeupNet(**model_params)
-    trainer = MakeupNetTrainer(model, dataset, **trainer_params)
+    if args.config is not None:
+        config = load_config(args.config)
+        transform = make_transform(config["model"]["image_size"])
+        dataset = MakeupDataset(**config["dataset"], transform=transform)
+        model = MakeupNet(**config["model"])
+        trainer = MakeupNetTrainer(model, dataset, **config["trainer"])
+    else:
+        transform = make_transform(args.image_size)
+        dataset = MakeupDataset(**get_dataset_args(args), transform=transform)
+        model = MakeupNet(**get_model_args(args))
+        trainer = MakeupNetTrainer(model, dataset, **get_trainer_args(args))
 
     # Train MakeupNet
     trainer.run(num_epochs=args.num_epochs, save_results=args.save_results)
@@ -172,68 +140,84 @@ if __name__ == "__main__":
 
 
     # Initialize parser and add arguments
-    parser = argparse.ArgumentParser(description="train MakeupNet on MakeupDataset.")
+    parser = argparse.ArgumentParser(description="train MakeupNet on MakeupDataset using MakeupNetTrainer.")
 
-    parser.add_argument("--random_seed", type=int, default=RANDOM_SEED,
-        help="random seed (0 uses pytorch initial seed).")
+    parser.add_argument("--config", type=str,
+        help="The index of the configurations of dataset, model, and trainer as defined in 'config.yaml'. "
+             "This will override all given args for dataset, model, and trainer. "
+             "In other words, only use --config along with --random_seed, --num_epochs, and --save_results.")
 
-    ### Dataset & Model Args ###
+    parser.add_argument("--random_seed", type=int, default=123,
+        help="random seed.")
+
+    ### Dataset Args ###
     parser.add_argument("--dataset_dir", type=str, default=DATASET_DIR,
         help="directory of the makeup dataset.")
     parser.add_argument("--with_landmarks", action="store_true",
         help="use faces landmarks in training as well.")
-    parser.add_argument("--image_size", type=positive(int), default=IMAGE_SIZE,
-        help="resize images to be of dimensions (image_size x image_size).")
 
-    parser.add_argument("--image_channels", type=positive(int), default=NUM_CHANNELS,
-        help="number of image channels in the dataset.")
-    parser.add_argument("--num_latents", type=positive(int), default=NUM_LATENTS,
+    ### Model Args ###
+    parser.add_argument("--num_latents", type=positive(int), default=128,
         help="number of latent factors from which an image will be generated.")
-    parser.add_argument("--num_features", type=positive(int), default=NUM_FEATURES,
+    parser.add_argument("--num_features", type=positive(int), default=64,
         help="number of features on the layers of the discriminator (and the generator as well).")
-    parser.add_argument("--gan_type", type=str.lower, default=GAN_TYPE,
+    parser.add_argument("--image_channels", type=positive(int), default=3,
+        help="number of image channels in the dataset.")
+    parser.add_argument("--image_size", type=positive(int), default=64,
+        help="resize images to be of dimensions (image_size x image_size).")
+    parser.add_argument("--gan_type", type=str.lower, default="gan",
         choices=("gan", "wgan", "wgan-gp"),
         help="type of gan among GAN (default), WGAN (Wasserstein GAN), and WGAN-GP (WGAN with gradient penalty).")
 
     ### Trainer Args ###
-    parser.add_argument("--trainer_name", type=str, default=TRAINER_NAME,
+    parser.add_argument("--trainer_name", type=str, default="trainer",
         help="name of the model trainer (which is also the name of your experiment).")
-    parser.add_argument("--results_dir", type=str, default=RESULTS_DIR,
+    parser.add_argument("--results_dir", type=str, default="results/",
         help="directory where the results for each run will be saved.")
-    parser.add_argument("--load_model", type=str, default=LOAD_MODEL_PATH,
+    parser.add_argument("--load_model", type=str,
         help="the path of the file where the model will be loaded and experiments will be saved.")
     
-    parser.add_argument("--num_gpu", type=nonnegative(int), default=NUM_GPU,
+    parser.add_argument("--num_gpu", type=nonnegative(int), default=0,
         help="number of GPUs to use, if any.")
-    parser.add_argument("--num_workers", type=nonnegative(int), default=NUM_WORKERS,
+    parser.add_argument("--num_workers", type=nonnegative(int), default=0,
         help="number of workers that will be loading the dataset.")
-    parser.add_argument("--batch_size", type=positive(int), default=BATCH_SIZE,
+    parser.add_argument("--batch_size", type=positive(int), default=4,
         help="size of the batch sample from the dataset.")
 
-    parser.add_argument("--optimizer", type=str.lower, default=OPTIMIZER_NAME,
+    parser.add_argument("--D_optimizer", type=str.lower, default="sgd",
         help="the name of the optimizer used for training (SGD, Adam, RMSProp)",
         choices=("sgd", "adam", "rmsprop"),)
-    parser.add_argument("--learning_rate", type=float, default=LEARNING_RATE,
+    parser.add_argument("--D_lr", type=float, default=1.0e-4,
         help="the learning rate, which controls the size of the optimization update.")
-    parser.add_argument("--momentum", type=positive(float), default=MOMENTUM,
+    parser.add_argument("--D_momentum", type=positive(float), default=0.0,
         help="used in SGD and RMSProp optimizers.")
-    parser.add_argument("--betas", type=float, nargs=2, default=BETAS,
+    parser.add_argument("--D_betas", type=float, nargs=2, default=(0.9, 0.999),
         help="used in Adam optimizer (see torch.optim.Adam for details).")
 
-    parser.add_argument("--D_iters", type=positive(int), default=D_ITERS,
+    parser.add_argument("--G_optimizer", type=str.lower, default="sgd",
+        help="the name of the optimizer used for training (SGD, Adam, RMSProp)",
+        choices=("sgd", "adam", "rmsprop"),)
+    parser.add_argument("--G_lr", type=float, default=1.0e-4,
+        help="the learning rate, which controls the size of the optimization update.")
+    parser.add_argument("--G_momentum", type=positive(float), default=0.0,
+        help="used in SGD and RMSProp optimizers.")
+    parser.add_argument("--G_betas", type=float, nargs=2, default=(0.9, 0.999),
+        help="used in Adam optimizer (see torch.optim.Adam for details).")
+
+    parser.add_argument("--D_iters", type=positive(int), default=5,
         help="number of iterations to train discriminator every batch.")
-    parser.add_argument("--clamp", type=float, nargs=2, default=CLAMP,
+    parser.add_argument("--clamp", type=float, nargs=2, default=(-0.01, 0.01),
         help="used in WGAN for clamping the weights of the discriminator.")
-    parser.add_argument("--gp_coeff", type=float, default=GRADIENT_PENALTY_COEFF,
+    parser.add_argument("--gp_coeff", type=float, default=10.0,
         help="a coefficient to multiply with the gradient penalty in the loss of WGAN-GP.")
 
-    parser.add_argument("--stats_interval", type=positive(int), default=STATS_REPORT_INTERVAL,
+    parser.add_argument("--stats_interval", type=positive(int), default=50,
         help="the interval in which a report of the training stats will be shown to the console.")
-    parser.add_argument("--progress_interval", type=positive(int), default=PROGRESS_CHECK_INTERVAL,
+    parser.add_argument("--generate_grid_interval", type=positive(int), default=200,
         help="the interval in which the progress of the generator will be checked and recorded.")
 
     ### Trainer.run() ###
-    parser.add_argument("--num_epochs", type=positive(int), default=NUM_EPOCHS,
+    parser.add_argument("--num_epochs", type=positive(int), default=1,
         help="number of training epochs (i.e. full runs on the dataset).")
     parser.add_argument("--save_results", action="store_true",
         help="save the results of the experiment.")
