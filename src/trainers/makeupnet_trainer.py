d5e13a8d5081d145a58b88f741c9579962c4dbe8
Abzollo <ma7aseem@gmail.com>
2019-10-02 22:42:11 +0400

    added makeupnet trainer + face morph

--- src/trainers/makeupnet_trainer.py
+++ src/trainers/makeupnet_trainer.py
@@ -1,20 +1,258 @@
 
 import torch
+import torch.nn.functional as F
 
 from .gan_trainer import GAN_Trainer
+from .base_trainer import BaseTrainer
+from .utils.init_utils import init_optim
+from .utils.gan_utils import *
+from .utils.face_morph.face_morph import face_morph
 
 
-class MakeupNetTrainer(GAN_Trainer):
+class _MakeupNetTrainer(GAN_Trainer):
     """The trainer for MakeupNet."""
 
-    def __init__(self, model, dataset,
-        **kwargs):
+    def __init__(self, model, dataset, **kwargs):
         """
-        Initializes GAN_Trainer.
+        Initializes MakeupNetTrainer.
 
         Args:
             model: The makeup net.
             dataset: The makeup dataset.
         """
-
         super().__init__(model, dataset, **kwargs)
+
+
+class MakeupNetTrainer(BaseTrainer):
+    """The trainer for MakeupNet."""
+
+    def __init__(self, model, dataset,
+                 D_optim_config={},
+                 G_optim_config={},
+                 D_iters=5,
+                 clamp=(-0.01, 0.01),
+                 gp_coeff=10.,
+                 stats_interval=50,
+                 **kwargs):
+        """
+        Initializes MakeupNetTrainer.
+
+        Args:
+            model: The makeup net.
+            dataset: The makeup dataset.
+        """
+        super().__init__(model, dataset, **kwargs)
+
+        self.D_iters = D_iters
+        self.clamp = clamp
+        self.gp_coeff = gp_coeff
+        self.stats_interval = stats_interval
+
+        # Initialize optimizers for generator and discriminator
+        self.optims = {
+            "applier": {
+                "D": init_optim(self.model.applier.D.parameters(), **D_optim_config),
+                "G": init_optim(self.model.applier.G.parameters(), **G_optim_config),
+            },
+            "remover": {
+                "D": init_optim(self.model.remover.D.parameters(), **D_optim_config),
+                "G": init_optim(self.model.remover.G.parameters(), **G_optim_config),
+            },
+            "style": {
+                "D": init_optim(self.model.style_D.parameters(), **D_optim_config),
+                # "G": init_optim([]),
+            }
+        }
+
+        # Initialize data dict
+        nan = torch.tensor(float("nan"))
+        self._data = {
+            "D_loss": [nan],
+            "G_loss": [nan],
+        }
+
+
+    def optims_zero_grad(self, D_or_G):
+        for optim in self.optims:
+            if D_or_G in optim:
+                optim[D_or_G].zero_grad()
+
+
+    def optims_step(self, D_or_G):
+        for optim in self.optims:
+            if D_or_G in optim:
+                optim[D_or_G].step()
+
+
+    def train_step(self, sample):
+
+        print("Step: %d" % self.iters)
+
+        makeup_applier = self.model.applier
+        makeup_remover = self.model.remover
+        makeup_style_D = self.model.style_D
+        
+        ### Sampling ###
+
+        # Sample from dataset
+        real_makeup = sample["after"].to(self.device)
+        real_nomakeup = sample["before"].to(self.device)
+        real_makeup_lm = sample["landmarks"]["after"]
+        real_nomakeup_lm = sample["landmarks"]["before"]
+
+        # Sample from generators @TODO: requires_grad?
+        with torch.no_grad():
+            fake_makeup = makeup_applier.G(real_nomakeup, real_makeup).detach()
+            fake_nomakeup = makeup_remover.G(real_makeup).detach()
+
+        # Morph makeup face to nomakeup face's facial structure for style loss calculation
+        mask, morphed_real_makeup = self.morph_makeup(real_makeup, real_nomakeup,
+                                                      real_makeup_lm, real_nomakeup_lm)
+        # Prepare real same style pair vs. fake same style pair
+        real_style_pair = (mask * real_makeup, mask * morphed_real_makeup)
+        fake_style_pair = (mask * real_makeup, mask * fake_makeup)
+
+        ### D step ###
+
+        # Zero gradients and loss
+        self.optims_zero_grad("D")
+
+        # Initialize D's loss
+        D_loss = 0.
+
+        # Adversarial loss for makeup domain
+        D_loss += 0.1 * get_D_loss(makeup_applier.D, real_makeup, fake_makeup,
+                                   gan_type=self.model.gan_type, gp_coeff=self.gp_coeff)
+
+        # Adversarial loss for no-makeup domain
+        D_loss += 0.1 * get_D_loss(makeup_remover.D, real_nomakeup, fake_nomakeup,
+                                   gan_type=self.model.gan_type, gp_coeff=self.gp_coeff)
+
+        # "Adversarial" loss for style domain (@XXX: gan_type shouldn't involve grad norm)
+        D_loss += 0.1 * get_D_loss(makeup_style_D, real_style_pair, fake_style_pair,
+                                   gan_type="gan", gp_coeff=self.gp_coeff)
+        
+        # Calculate gradients
+        D_loss.backward()
+
+        # Make a step of minimizing D's loss
+        self.optims_step("D")
+
+        ### End of D step ###
+
+        self._data["D_loss"].append(D_loss)
+
+
+        # Train generator if we trained discriminator D_iters time
+        if self.iters % self.D_iters == 0:
+
+            ### Sampling ###
+            
+            # Sample from generators
+            fake_makeup = makeup_applier.G(real_nomakeup, real_makeup)
+            fake_nomakeup = makeup_remover.G(real_makeup)
+
+
+            ### G step ###
+
+            # Zero gradients
+            self.optims_zero_grad("G")
+
+            # Initialize G's loss
+            G_loss = 0.
+
+            # Adversarial loss for makeup domain
+            G_loss += 0.1 * get_G_loss(makeup_applier.D, fake_makeup, gan_type=self.model.gan_type)
+            
+            # Adversarial loss for no-makeup domain
+            G_loss += 0.1 * get_G_loss(makeup_remover.D, fake_nomakeup, gan_type=self.model.gan_type)
+            
+            # Adversarial loss for style domain (@XXX: same as style's D_loss)
+            G_loss += 0.1 * get_G_loss(makeup_style_D, fake_style_pair, gan_type="gan")
+            
+            # Identity loss
+            G_loss += F.l1_loss(real_nomakeup, makeup_remover.G(fake_makeup))
+            
+            # Style loss (i.e. style is preserved in fake_makeup and well-removed in fake_nomakeup)
+            G_loss += F.l1_loss(real_makeup, makeup_applier.G(fake_nomakeup, fake_makeup))
+            
+            # Extra sparsity-inducing regularization for makeup mask
+            G_loss += 0.1 * F.l1_loss(real_nomakeup, fake_makeup)
+
+            # Calculate gradients
+            G_loss.backward()
+
+            # Make a step of minimizing G's loss
+            self.optims_step("G")
+
+            ### End of G step ###
+
+            self._data["G_loss"].append(G_loss)
+
+        else:
+            self._data["G_loss"].append(self._data["G_loss"][-1])
+
+
+    def morph_makeup(self, real_makeup, real_nomakeup, real_makeup_lm, real_nomakeup_lm):
+
+        tensor2D_to_points = lambda t: [(p[0].item(), p[1].item()) for p in t]
+        torch_to_numpy = lambda t: t.permute(1, 2, 0).numpy()
+        numpy_to_torch = lambda t: torch.from_numpy(t).permute(2, 0, 1)
+
+        batch_size = real_makeup.size()[0]
+        mask = torch.ones([batch_size, 1, 1, 1]).to(real_makeup)
+        morphed_batch = []
+
+        for i in range(batch_size):
+            # Zero mask for no landmarks
+            if real_makeup_lm[i].sum() == 0 or real_nomakeup_lm[i].sum() == 0:
+                morphed_batch.append(torch.zeros_like(real_nomakeup[i]))
+                mask[i] = 0
+            else:
+                morphed = face_morph(torch_to_numpy(real_makeup[i]),
+                                     torch_to_numpy(real_nomakeup[i]),
+                                     tensor2D_to_points(real_makeup_lm[i]),
+                                     tensor2D_to_points(real_nomakeup_lm[i]))
+                morphed_batch.append(numpy_to_torch(morphed))
+        
+        return mask, torch.stack(morphed_batch).to(real_makeup)
+
+
+    def checkpoint(self, epoch, num_epochs, batch, num_batches):
+        # Report training stats
+        if batch % self.stats_interval == 0:
+            self.report_training_stats(batch, num_batches, epoch, num_epochs)
+
+
+    def report_training_stats(self, epoch, num_epochs, batch, num_batches, precision=3):
+        """
+        Reports/prints the training stats to the console.
+
+        Args:
+            epoch: Current epoch.
+            num_epochs: Max number of epochs.
+            batch: Index of the current batch.
+            num_batches: Max number of batches.
+            precision: Precision of the float numbers reported.
+        """
+
+        report = \
+            "[{epoch}/{num_epochs}][{batch}/{num_batches}]\t" \
+            "Loss of D = {D_loss:.{p}f}\t" \
+            "Loss of G = {G_loss:.{p}f}\t" \
+            "D(x) = {D_of_x:.{p}f}\t" \
+            "D(G(z)) = {D_of_G_z1:.{p}f} / {D_of_G_z2:.{p}f}"
+
+        stats = {
+            "epoch": epoch,
+            "num_epochs": num_epochs,
+            "batch": batch,
+            "num_batches": num_batches,
+            "D_loss": self._data["D_loss"][-1],
+            "G_loss": self._data["G_loss"][-1],
+            "p": precision,
+        }
+
+        print(report.format(**stats))
+
+
---
+++ src/trainers/utils/face_morph/face_morph.py
@@ -0,0 +1,245 @@
+
+import pickle
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from PIL import Image, ImageDraw
+from face_recognition import face_landmarks
+
+
+dict_to_list = lambda d: [x for l in d.values() for x in l]
+
+def delauney(points, img, draw=False):
+    """
+    Finds Delauney's triangulation of the list of points
+    in an img, and draw it if necessary.
+    
+    Args:
+        points: A list of 2D points as tuples.
+        img: The image of containing the points as a PIL or numpy.
+        draw: Draw the triangulation on the img if True.
+        
+    """
+    
+    # Init subdiv and insert points
+    _, h, w =  np.array(img).shape
+    xs, ys = zip(*points)
+    rect_x = min(0, min(xs))
+    rect_y  = min(0, min(ys))
+    rect_w = max(w, max(xs) - rect_x) + 1
+    rect_h = max(h, max(ys) - rect_y) + 1
+    
+    rect = (rect_x, rect_y, rect_w, rect_h)
+    subdiv = cv2.Subdiv2D(rect)
+    subdiv.insert(points)
+
+    triangles = []
+    for t in subdiv.getTriangleList():
+        # Get triangle point
+        t = np.int32(t)
+        p1 = (t[0], t[1])
+        p2 = (t[2], t[3])
+        p3 = (t[4], t[5])
+        t = tuple(sorted([p1, p2, p3]))
+        triangles.append(t)
+
+    return sorted(triangles)
+
+
+def warp_triangle(t1, t2, img1, img2, alpha=0.8):
+    """
+    Warps triangle1 in img1 to triangle2 in img2 by a factor of alpha.
+    
+    Args:
+        t1: A list of 3 points (or tuples) of the triangle in img1.
+        t2: A list of 3 points (or tuples) of the triangle in img2.
+        img1: Tthe source image as a numpy array.
+        img2: The destination image as a numpy array.
+        alpha: Interpolation factor of the warp.
+    """
+    
+    # Convert source and destination triangle to numpy
+    t1 = np.float32(t1)
+    t2 = np.float32(t2)
+
+    # Get the bounding rectangles (patches) of the triangles
+    x1, y1, w1, h1 = cv2.boundingRect(t1)
+    x2, y2, w2, h2 = cv2.boundingRect(t2)
+    
+    # Sometimes, landmarks reside slightly outside the image... @XXX: hacky.
+    x1, y1, x2, y2 = max(0, x1), max(0, y1), max(0, x2), max(0, y2)
+    
+    # Offset triangles' coordinates by the bounding rect's coordinates
+    t1[:, 0] = t1[:, 0] - x1
+    t1[:, 1] = t1[:, 1] - y1
+    t2[:, 0] = t2[:, 0] - x2
+    t2[:, 1] = t2[:, 1] - y2
+    
+    # Get the rectangles from the images
+    patch1 = img1[y1:y1+h1, x1:x1+w1].copy()
+    patch2 = img2[y2:y2+h2, x2:x2+w2].copy()
+
+    # If one of the patches have length 0 in any dimension, skip this warp
+    if 0 in patch1.shape or 0 in patch2.shape:
+        return
+
+    # Get the affine transformation between the triangles in the new coordinates
+    affine1to2 = cv2.getAffineTransform(t1, t2)
+    
+    # Affine-warp patch1 to patch2
+    #print("t1:", t1)
+    #print(x1, y1, w1, h1)
+    #print("patch1.shape:", patch1.shape)
+    patch1_warped = cv2.warpAffine(patch1, affine1to2, (w2,h2),
+                                   borderMode=cv2.BORDER_REFLECT_101)
+    
+    # Crop out points outside image on the max side. @XXX: hacky but ok.
+    if patch1_warped.shape != patch2.shape:
+        patch1_warped = patch1_warped[0:patch2.shape[0], 0:patch2.shape[1]]
+        
+    # Create a mask to get a t2-like triangle out of patch1_warped
+    mask = cv2.fillConvexPoly(np.zeros_like(patch2), np.int32(t2), (1.0, 1.0, 1.0))
+    
+    # Now interpolate the warped t1 in patch1_warped to t2 by a factor of alpha
+    mask = alpha * np.float32(mask)
+    warped_patch = mask * patch1_warped + (1 - mask) * patch2
+    
+    # Paste the warped patch to img2 in place of patch2
+    img2[y2:y2+h2, x2:x2+w2] = warped_patch
+    
+    return t1, t2, patch1, patch2, patch1_warped
+
+
+
+def face_morph(img1, img2, landmarks1=None, landmarks2=None, alpha=0.95):
+    """
+    Morph face in img1 to face in img2 by a factor of alpha, given their landmarks.
+
+    Args:
+        img1: The source image as a PIL image or a numpy array.
+        img2: The destination image as a PIL image or a numpy array.
+        landmarks1: List of landmarks points from img1.
+        landmarks2: List of landmarks points from img2.
+        alpha: Factor of interpolation from face in img1 to face in img2.
+    """
+
+    # Convert PIL images to np arrays
+    img_array1, img_array2 = np.array(img1), np.array(img2)
+
+    # Find landmarks if none were given
+    if landmarks1 is None: landmarks1 = find_landmarks(img1)
+    if landmarks2 is None: landmarks2 = find_landmarks(img2)
+
+    # Create a map that links the vertices of the two landmarks together
+    points_map = dict(zip(landmarks1, landmarks2))
+
+    # Warp each triangle in img1 to its corresponding one in img2
+    for t1 in delauney(landmarks1, img1):
+        t2 = tuple(map(lambda p: points_map[p], t1))
+        warp_triangle(t1, t2, img_array1, img_array2, alpha=alpha)
+
+    return img_array2
+
+
+def face_morph_video(filename, img1, img2, landmarks1=None, landmarks2=None):
+
+    # Convert PIL images to np arrays
+    img_array1, img_array2 = np.array(img1), np.array(img2)
+
+    # Find landmarks if none were given
+    if landmarks1 is None: landmarks1 = find_landmarks(img1)
+    if landmarks2 is None: landmarks2 = find_landmarks(img2)
+
+    # Create a map that links the vertices of the two landmarks together
+    points_map = dict(zip(landmarks1, landmarks2))
+
+    # Find Delauney triangles of both landmarks
+    triangles1 = delauney(landmarks1, img1)
+    # triangles2 = delauney(landmarks2, img2)
+
+    # Prepare figure and frames of morphed images
+    fig = plt.figure()
+    plt.axis('off')
+    morphed_imgs = []
+
+    # For each morphing factor `alpha`...
+    step = 0.05
+    for alpha in np.arange(0, 1.0, step):
+        
+        # Start morphed img from the original img2
+        morphed_img = img_array2.copy()
+        
+        # Warp each triangle in img1 to its corresponding one in img2
+        for t1 in triangles1:
+            t2 = tuple(map(lambda p: points_map[p], t1))
+            warp_triangle(t1, t2, img_array1, morphed_img, alpha=alpha)
+        
+        # save frame of morphed image to animate later
+        morphed_imgs.append([plt.imshow(morphed_img, animated=True)])
+
+    # Make animation and play
+    ani = animation.ArtistAnimation(fig, morphed_imgs,
+                                    interval=200, blit=True, repeat_delay=1000)
+    ani.save(filename)
+
+
+def find_landmarks(img):
+    landmarks_found = face_landmarks(np.array(img))
+    if len(landmarks_found) > 0:
+        return dict_to_list(landmarks_found[0])
+    else:
+        raise Exception("Failed to find face landmarks for one of the images.")
+
+
+#############################################
+
+def test(test_num, reverse=False, load_landmarks=False):
+
+    # Just a test
+    img1_name = f"img/{test_num}-before.jpeg"
+    img2_name = f"img/{test_num}-after.jpeg"
+    landmarks1_name = f"img/landmarks/{test_num}-before.pickle"
+    landmarks2_name = f"img/landmarks/{test_num}-after.pickle"
+
+    # Reverse morph direction (morph 2 -> 1 instead of 1 -> 2)
+    if reverse:
+        img1_name, img2_name = img2_name, img1_name
+        landmarks1_name, landmarks2_name = landmarks2_name, landmarks1_name
+        test_num += "_apply"
+    else:
+        test_num += "_remove"
+
+
+    # Load images
+    img1 = Image.open(img1_name)
+    img2 = Image.open(img2_name)
+
+    if load_landmarks:
+        # Load landmarks
+        with open(landmarks1_name, "rb") as f:
+            landmarks1 = dict_to_list(pickle.load(f))
+        with open(landmarks2_name, "rb") as f:
+            landmarks2 = dict_to_list(pickle.load(f))
+    else:
+        # Extract landmarks
+        landmarks1 = find_landmarks(img1)
+        landmarks2 = find_landmarks(img2)
+
+    # Morph face
+    makeup_process = "apply" if reverse else "remove"
+    filename = f"morphing_face_{test_num}_{makeup_process}.mp4"
+    face_morph_video(filename, img1, img2, landmarks1, landmarks2)
+
+
+def main():
+    test("00000")
+    test("00000", reverse=True, load_landmarks=True)
+    test("00003", reverse=True)
+    test("00003", load_landmarks=True)
+
+
+if __name__ == '__main__':
+    main()
+
