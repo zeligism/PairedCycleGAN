3d9bb452b27fe9929808aaa2050c6b38022ceb38
Abzollo <ma7aseem@gmail.com>
2019-10-05 02:19:01 +0400

    moved TODO.sh to root, made it more versatile

---
+++ TODO.sh
@@ -0,0 +1,3 @@
+# bash TODO.sh
+find . -type f -name "*.py" | xargs grep -n --color @TODO
+find . -type f -name "*.py" | xargs grep -n --color @XXX
--- src/TODO.sh
+++
@@ -1,3 +0,0 @@
-# bash TODO.sh
-grep -n --color @TODO ./*.py ./*/*.py ./*/*/*.py ./*/*/*/*.py
-grep -n --color @XXX  ./*.py ./*/*.py ./*/*/*.py ./*/*/*/*.py
