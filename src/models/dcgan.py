3153ddf77a2155c828c8458605d1817d9d49d6b0
Abdulla Al Mansoori <abdulla.almansoori@nyu.edu>
2020-08-31 19:56:03 +0400

    completely changed the design of dcgan (based on my other repo flexidcgan, which is still a dcgan but better in some ways)

--- src/models/dcgan.py
+++ src/models/dcgan.py
@@ -1,18 +1,20 @@
 
 import torch.nn as nn
 
-from math import log2
-
 
 class DCGAN(nn.Module):
     """Deep Convolutional Generative Adversarial Network"""
 
     def __init__(self,
-        num_latents=128,
-        num_features=64,
-        image_channels=3,
-        image_size=64,
-        gan_type="gan"):
+                 num_latents=100,
+                 num_features=64,
+                 image_channels=3,
+                 image_size=64,
+                 gan_type="gan",
+                 fully_convolutional=True,
+                 activation=None,
+                 use_batchnorm=True,
+                 use_spectralnorm=False):
         """
         Initializes DCGAN.
 
@@ -22,7 +24,6 @@ class DCGAN(nn.Module):
             image_channels: Number of channels in the input image.
             image_size: Size (i.e. height or width) of image.
             gan_type: Type of GAN (e.g. "gan" or "wgan-gp").
-
         """
         super().__init__()
 
@@ -31,12 +32,21 @@ class DCGAN(nn.Module):
         self.image_channels = image_channels
         self.image_size = image_size
         self.gan_type = gan_type
+        self.fully_convolutional = fully_convolutional
+        self.activation = activation
+        self.use_batchnorm = use_batchnorm
+        self.use_spectralnorm = use_spectralnorm
 
         D_params = {
+            "num_latents": 1,
             "num_features": num_features,
             "image_channels": image_channels,
             "image_size": image_size,
             "gan_type": gan_type,
+            "fully_convolutional": fully_convolutional,
+            "activation": activation,
+            "use_batchnorm": use_batchnorm,
+            "use_spectralnorm": use_spectralnorm,
         }
         G_params = {
             "num_latents": num_latents,
@@ -44,50 +54,14 @@ class DCGAN(nn.Module):
             "image_channels": image_channels,
             "image_size": image_size,
             "gan_type": gan_type,
+            "fully_convolutional": fully_convolutional,
+            "activation": activation,
+            "use_batchnorm": use_batchnorm,
+            "use_spectralnorm": use_spectralnorm,
         }
 
         self.D = DCGAN_Discriminator(**D_params)
         self.G = DCGAN_Generator(**G_params)
-
-
-class DCGAN_Discriminator(nn.Module):
-    """The discriminator of a DCGAN"""
-
-    def __init__(self,
-                 num_features=64,
-                 image_channels=3,
-                 image_size=64,
-                 gan_type="gan",
-                 max_features=512):
-        super().__init__()
-
-        using_gradient_penalty = gan_type == "wgan-gp"
-        use_batchnorm = not using_gradient_penalty
-
-        # Count number of layers (including input) and calculate feature sizes
-        num_layers = int(round(log2(image_size // 4)))
-        features = [min(num_features * 2**i, max_features) for i in range(num_layers)]
-
-        modules = []
-
-        # Input layer
-        modules += [DCGAN_DiscriminatorBlock(image_channels, features[0])]
-
-        # Intermediate layers
-        for in_features, out_features in zip(features, features[1:]):
-            modules += [DCGAN_DiscriminatorBlock(in_features, out_features, use_batchnorm=use_batchnorm)]
-        
-        # Output layer (feature_size = 4 -> 1)
-        modules += [nn.Conv2d(features[-1], 1, 4, bias=False)]
-
-        if gan_type == "gan":
-            modules += [nn.Sigmoid()]
-
-        self.main = nn.Sequential(*modules)
-
-
-    def forward(self, inputs):
-        return self.main(inputs).view(-1)
 
 
 class DCGAN_DiscriminatorBlock(nn.Module):
@@ -97,58 +71,23 @@ class DCGAN_DiscriminatorBlock(nn.Module):
     e.g. if input is [in_channels, 64, 64], output will be [out_channels, 32, 32].
     """
 
-    def __init__(self, in_channels, out_channels, stride=2, padding=1, use_batchnorm=False):
+    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1,
+                 use_batchnorm=True, use_spectralnorm=False, activation=None):
         super().__init__()
 
-        modules = []
-
-        modules += [nn.Conv2d(in_channels, out_channels, 4, stride=2, padding=1, bias=False)]
-        modules += [nn.BatchNorm2d(out_channels)] if use_batchnorm else []
-        modules += [nn.LeakyReLU(0.2, inplace=True)]
-
-        self.main = nn.Sequential(*modules)
-
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                              stride=stride, padding=padding, bias=False)
+        if use_spectralnorm:
+            self.conv = nn.utils.spectral_norm(self.conv)
+        self.batchnorm = nn.BatchNorm2d(out_channels) if use_batchnorm else None
+        self.activation = nn.LeakyReLU(0.2, inplace=True) if activation is None else activation()
 
     def forward(self, x):
-        return self.main(x)
-
-
-class DCGAN_Generator(nn.Module):
-    """The generator of a DCGAN"""
-
-    def __init__(self,
-                 num_latents,
-                 num_features=64,
-                 image_channels=3,
-                 image_size=64,
-                 gan_type="gan",
-                 max_features=512):
-        super().__init__()
-
-        # Count number of layers (including input) and calculate feature sizes
-        num_layers = int(round(log2(image_size // 4)))
-        features = [min(num_features * 2**i, max_features) for i in range(num_layers)][::-1]
-
-        modules = []
-
-        # Input layer
-        modules += [DCGAN_GeneratorBlock(num_latents, features[0], stride=1, padding=0)]
-
-        # Intermediate layers
-        for in_features, out_features in zip(features, features[1:]):
-            modules += [DCGAN_GeneratorBlock(in_features, out_features)]
-        
-        # Output layer
-        modules += [nn.ConvTranspose2d(features[-1], image_channels, 4,
-                                       stride=2, padding=1, bias=False)]
-        modules += [nn.Tanh()]
-
-        self.main = nn.Sequential(*modules)
-
-
-    def forward(self, inputs):
-        inputs = inputs.view(*inputs.size(), 1, 1)  # add H and W dimensions
-        return self.main(inputs)
+        x = self.conv(x)
+        if self.batchnorm:
+            x = self.batchnorm(x)
+        x = self.activation(x)
+        return x
 
 
 class DCGAN_GeneratorBlock(nn.Module):
@@ -158,17 +97,176 @@ class DCGAN_GeneratorBlock(nn.Module):
     e.g. if input is [in_channels, 32, 32], output will be [out_channels, 64, 64].
     """
 
-    def __init__(self, in_channels, out_channels, stride=2, padding=1):
+    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1,
+                 use_batchnorm=True, use_spectralnorm=False, activation=None):
         super().__init__()
 
-        self.main = nn.Sequential(
-            nn.ConvTranspose2d(in_channels, out_channels, 4,
-                               stride=stride, padding=padding, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-
+        self.convT = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size,
+                                        stride=stride, padding=padding, bias=False)
+        if use_spectralnorm:
+            self.convT = nn.utils.spectral_norm(self.convT)
+        self.batchnorm = nn.BatchNorm2d(out_channels) if use_batchnorm else None
+        self.activation = nn.LeakyReLU(0.2, inplace=True) if activation is None else activation() # XXX: ReLU?
 
     def forward(self, x):
-        return self.main(x)
+        x = self.convT(x)
+        if self.batchnorm:
+            x = self.batchnorm(x)
+        x = self.activation(x)
+        return x
+
+
+class DCGAN_Discriminator(nn.Module):
+    """The discriminator of a DCGAN"""
+
+    def __init__(self,
+                 num_latents=1,
+                 num_features=64,
+                 image_channels=3,
+                 image_size=64,
+                 gan_type="gan",
+                 max_features=512,
+                 fully_convolutional=True,
+                 activation=None,
+                 use_batchnorm=True,
+                 use_spectralnorm=False,
+                 D_block=DCGAN_DiscriminatorBlock):
+        super().__init__()
+
+        # Reset batchnorm to False if using gradient penalty
+        if use_batchnorm:
+            using_gradient_penalty = gan_type == "wgan-gp"
+            use_batchnorm = not using_gradient_penalty
+
+        block_config = {
+            "activation": activation,
+            "use_batchnorm": use_batchnorm,
+            "use_spectralnorm": use_spectralnorm
+        }
+
+        # Calculate intermediate image sizes
+        image_sizes = [image_size]
+        while image_sizes[-1] > 5:
+            image_sizes.append(image_sizes[-1] // 2)
+        latent_kernel = image_sizes[-1]  # should be either 3, 4, or 5
+        num_layers = len(image_sizes) - 1
+
+        # Calculate feature sizes
+        features = [min(num_features * 2**i, max_features) for i in range(num_layers)]
+
+        # Input layer
+        self.input_layer = D_block(image_channels, features[0], **block_config)
+
+        # Intermediate layers
+        self.main_layers = nn.Sequential(*[
+            D_block(in_features, out_features, **block_config)
+            for in_features, out_features in zip(features, features[1:])
+        ])
+
+        # Output layer (feature_size = 3, 4, or 5 -> 1)
+        if fully_convolutional:
+            self.output_layer = nn.Sequential(
+                nn.Conv2d(features[-1], num_latents, latent_kernel, bias=False),
+                nn.Flatten(),
+            )
+        else:
+            self.output_layer = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(features[-1] * latent_kernel**2, num_latents, bias=False)
+            )
+
+        # Add sigmoid activation if using regular GAN loss
+        self.output_activation = nn.Sigmoid() if gan_type == "gan" else None
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = self.main_layers(x)
+        x = self.output_layer(x)
+        if self.output_activation:
+            x = self.output_activation(x)
+        # Remove H and W dimensions, infer channels dim (remove if 1)
+        x = x.view(x.size(0), -1).squeeze(1)
+        return x
+
+
+class DCGAN_Generator(nn.Module):
+    """The generator of a DCGAN"""
+
+    def __init__(self,
+                 num_latents=100,
+                 num_features=64,
+                 image_channels=3,
+                 image_size=64,
+                 gan_type="gan",
+                 max_features=512,
+                 fully_convolutional=True,
+                 activation=None,
+                 use_batchnorm=True,
+                 use_spectralnorm=False,
+                 G_block=DCGAN_GeneratorBlock):
+        super().__init__()
+
+        block_config = {
+            "activation": activation,
+            "use_batchnorm": use_batchnorm,
+            "use_spectralnorm": use_spectralnorm
+        }
+
+        # Calculate intermediate image sizes
+        image_sizes = [image_size]
+        while image_sizes[-1] > 5:
+            image_sizes.append(image_sizes[-1] // 2)
+        latent_kernel = image_sizes[-1]  # should be either 3, 4, or 5
+        num_layers = len(image_sizes) - 1
+
+        # Calculate feature sizes
+        features = [min(num_features * 2**i, max_features) for i in range(num_layers)]
+
+        # Reverse order of image sizes and features for generator
+        image_sizes = image_sizes[::-1]
+        features = features[::-1]
+
+        # Input layer
+        if fully_convolutional:
+            self.input_layer = G_block(num_latents, features[0], kernel_size=latent_kernel,
+                                       stride=1, padding=0, **block_config)
+        else:
+            self.input_layer = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(num_latents, features[0] * image_sizes[0]**2, bias=False),
+                View(features[0], image_sizes[0], image_sizes[0])
+            )
+
+        # Intermediate layers
+        self.main_layers = nn.Sequential(*[
+            G_block(in_features, out_features, kernel_size=4+(expected_size%2), **block_config)
+            for in_features, out_features, expected_size in zip(features, features[1:], image_sizes[1:])
+        ])
+
+        # Output layer
+        self.output_layer = nn.ConvTranspose2d(features[-1], image_channels, kernel_size=4+(image_size%2),
+                                               stride=2, padding=1, bias=False)
+        self.output_activation = nn.Tanh()
+
+    def forward(self, x):
+        # Add H and W dimensions, infer channels dim (add if none)
+        x = x.view(x.size(0), -1, 1, 1)
+        x = self.input_layer(x)
+        x = self.main_layers(x)
+        x = self.output_layer(x)
+        x = self.output_activation(x)
+        return x
+
+
+class View(nn.Module):
+    def __init__(self, *shape, including_batch=False):
+        super().__init__()
+        self.shape = shape
+        self.including_batch = including_batch
+    
+    def forward(self, x):
+        if self.including_batch:
+            return x.view(*self.shape)
+        else:
+            return x.view(x.size(0), *self.shape)
 
